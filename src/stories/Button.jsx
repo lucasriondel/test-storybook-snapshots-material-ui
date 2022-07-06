eff5ec3b864dc437431d580a7a69168cd414bf76
Lucas Riondel <lucasrndl@gmail.com>
2022-07-06 15:30:42 +0200

    feat: setup working

---
+++ src/stories/Button.jsx
@@ -0,0 +1,54 @@
+import { styled } from '@mui/material';
+import PropTypes from 'prop-types';
+import './button.css';
+
+const ButtonContainer = styled('button')`
+  background-color: red;
+`
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  return (
+    <ButtonContainer
+      type="button"
+      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      style={backgroundColor && { backgroundColor }}
+      {...props}
+    >
+      {label}
+    </ButtonContainer>
+  );
+};
+
+Button.propTypes = {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary: PropTypes.bool,
+  /**
+   * What background color to use
+   */
+  backgroundColor: PropTypes.string,
+  /**
+   * How large should the button be?
+   */
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  /**
+   * Button contents
+   */
+  label: PropTypes.string.isRequired,
+  /**
+   * Optional click handler
+   */
+  onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  backgroundColor: null,
+  primary: false,
+  size: 'medium',
+  onClick: undefined,
+};
---
+++ src/storyshots.test.js
@@ -0,0 +1,8 @@
+import initStoryshots from "@storybook/addon-storyshots";
+// import initStoryshots, {
+//   multiSnapshotWithOptions,
+// } from "@storybook/addon-storyshots";
+
+initStoryshots({
+  //   test: multiSnapshotWithOptions({}),
+});
